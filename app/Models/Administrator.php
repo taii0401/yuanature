ac795f4d0c311f5bbfefa3c4abd7a275aa145b3b
taii0401 <taii0401@gmail.com>
2023-04-13 00:32:17 +0800

    'model管理者-取得全部資料'

--- app/Models/Administrator.php
+++ app/Models/Administrator.php
@@ -40,4 +40,63 @@ class Administrator extends Model
         }
         return $name;
     }
+
+    /**
+     * 取得資料
+     * @param  cond：搜尋條件
+     * @param  orderby：排序欄位
+     * @param  sort：排序-遞增、遞減
+     * @return data
+     */
+    public static function getAllDatas($cond=[],$orderby="id",$sort="asc")
+    {
+        $all_datas = $conds = $conds_in = $conds_like = [];
+        
+        //條件欄位
+		$cols = ["id","uuid","account","name","status","admin_group_id"];
+		foreach($cols as $col) {
+			if(isset($cond[$col])) {
+                if(in_array($col,["account","name"])) {
+                    $conds_like[$col] = $cond[$col];
+                } else {
+                    if(is_array($cond[$col])) {
+                        $conds_in[$col] = $cond[$col];
+                    } else if($cond[$col] != "") {
+                        if(is_numeric($cond[$col])) {
+                            $conds[$col] = (int)$cond[$col];
+                        } else {
+                            $conds[$col] = $cond[$col];
+                        }
+                    }
+                }
+			}
+		}
+        $all_datas = self::where($conds);
+        //搜尋條件
+        if(!empty($conds_in)) {
+            foreach($conds_in as $key => $val) {
+                $all_datas = $all_datas->whereIn($key,$val);
+            }
+        }
+        if(!empty($conds_like)) {
+            foreach($conds_like as $key => $val) {
+                $all_datas = $all_datas->where($key,"like","%".$val."%");
+            }
+        }
+        //關鍵字
+        if(isset($cond["keywords"]) && $cond["keywords"] != "") {
+            $keywords = $cond["keywords"];
+            $conds_or = array("account","name");
+            $all_datas = $all_datas->where(function ($query) use($conds_or,$keywords) {
+                foreach($conds_or as $value) {
+                    $query->orWhere($value,"like","%".$keywords."%");
+                }
+            });
+        }
+        //排序
+        $all_datas = $all_datas->orderBy($orderby,$sort);
+        //print_r($all_datas->toSql());
+
+        return $all_datas;
+    }
 }
