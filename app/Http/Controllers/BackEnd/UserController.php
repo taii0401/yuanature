ffadde62399fd7fa0b7c1eb8d80b5fde036c6f98
dilys <dilys@gastom.com.tw>
2023-04-17 18:30:00 +0800

    '會員列表-修改搜尋條件'

--- app/Http/Controllers/BackEnd/UserController.php
+++ app/Http/Controllers/BackEnd/UserController.php
@@ -21,9 +21,10 @@ class UserController extends Controller
         $input = $request->all();
         $assign_data = $list_data = $page_data = $option_data = [];
 
-        //搜尋條件-登入方式、是否驗證、排序
-        $option_data["register_type"] = WebCode::getCodeOptions("user_register");
-        $option_data["is_verified"] = ["" => "全部",WebUser::IS_VERIFIED_YES => WebUser::class::$isVerifiedName[WebUser::IS_VERIFIED_YES],WebUser::IS_VERIFIED_NO => WebUser::class::$isVerifiedName[WebUser::IS_VERIFIED_NO]];
+        //選單搜尋條件-登入方式、是否驗證、排序
+        $option_data["register_type"] = ["name" => "登入方式","data" => WebCode::getCodeOptions("user_register")];
+        $option_data["is_verified"] = ["name" => "是否驗證","data" => ["" => "全部",WebUser::IS_VERIFIED_YES => WebUser::class::$isVerifiedName[WebUser::IS_VERIFIED_YES],WebUser::IS_VERIFIED_NO => WebUser::class::$isVerifiedName[WebUser::IS_VERIFIED_NO]]];
+        $option_data["orderby"] = ["name" => "排序","data" => ["asc_created_at" => "建立時間 小 ~ 大","desc_created_at" => "建立時間 大 ~ 小"]];
         //取得目前頁數及搜尋條件
         $search_datas = ["page","orderby","keywords","register_type","is_verified"];
         $get_search_data = $this->getSearch($search_datas,$input);
@@ -34,8 +35,17 @@ class UserController extends Controller
         //標題
         $assign_data["title_txt"] = "會員列表";
 
+        //排序
+        $orderby_sort = "asc";
+        $orderby_col = "created_at";
+        if(isset($assign_data["orderby"]) && $assign_data["orderby"] != "") {
+            $orderby = $assign_data["orderby"];
+            $str = explode("_",$orderby);
+            $orderby_sort = isset($str[0])?$str[0]:$orderby_sort;
+            $orderby_col = isset($str[1])?str_replace($orderby_sort."_","",$orderby):$orderby_col;
+        }
         //取得所有資料
-        $all_datas = WebUser::getAllDatas($get_search_data["conds"]);
+        $all_datas = WebUser::getAllDatas($get_search_data["conds"],$orderby_col,$orderby_sort);
         //處理分頁資料
         $page_data = $this->getPage($page,$all_datas,$assign_data["search_get_url"]);
         $list_data = isset($page_data["list_data"])?$page_data["list_data"]:array();
