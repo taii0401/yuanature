d35fab7ce2f7567c706a904f809c41fff0c2ecd2
dilys <dilys@gastom.com.tw>
2023-04-26 10:20:57 +0800

    '訂單詳細頁面-訂單資料改為共用樣板'

--- resources/views/orders/data.blade.php
+++ resources/views/orders/data.blade.php
@@ -19,74 +19,7 @@
                 <div class="table-responsive" style="display:{{ @$datas["assign_data"]["cart_none"] }};">
                     <div id="msg_error" class="col-12 alert alert-danger" role="alert" style="display:{{ @$datas["assign_data"]["danger_none"] }};">交易失敗</div>
                     <div id="msg_success" class="col-12 alert alert-success" role="alert" style="display:{{ @$datas["assign_data"]["success_none"] }};">交易成功</div>
-                    <table class="table table-hover table-striped tm-table-striped-even mt-3"  style="vertical-align: middle;">
-                        <thead>
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">訂單編號：</th>
-                                <th style="color:#007bff">{{ @$datas["assign_data"]["serial"] }}</th>
-                            </tr>
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">訂購日期：</th>
-                                <th>{{ @$datas["assign_data"]["created_at_format"] }}</th>
-                            </tr>
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">收件人姓名：</th>
-                                <th>{{ @$datas["assign_data"]["name"] }}</th>
-                            </tr>
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">收件人電話：</th>
-                                <th>{{ @$datas["assign_data"]["phone"] }}</th>
-                            </tr>
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">收件人信箱：</th>
-                                <th>{{ @$datas["assign_data"]["email"] }}</th>
-                            </tr>
-                            @if(@$datas["assign_data"]["delivery"] == "home" && @$datas["assign_data"]["address"] != "")
-                                <tr>
-                                    <th class="text-center tm-bg-gray" height="50px">收件人地址：</th>
-                                    <th>{{ @$datas["assign_data"]["address"] }}</th>
-                                </tr>
-                            @endif
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">訂單狀態：</th>
-                                <th style="color:{{ @$datas["assign_data"]["status_color"] }}">{{ @$datas["assign_data"]["status_name"] }}</th>
-                            </tr>
-                            @if(@$datas["assign_data"]["status"] == "cancel")
-                                <tr>
-                                    <th class="text-center tm-bg-gray" height="50px">取消原因：</th>
-                                    <th>{{ @$datas["assign_data"]["cancel_name"] }}</th>
-                                </tr>
-                            @endif
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">配送方式：</th>
-                                <th style="color:{{ @$datas["assign_data"]["delivery_color"] }}">{{ @$datas["assign_data"]["delivery_name"] }}</th>
-                            </tr>
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">付款方式：</th>
-                                <th style="color:{{ @$datas["assign_data"]["payment_color"] }}">{{ @$datas["assign_data"]["payment_name"] }}</th>
-                            </tr>
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">訂購金額：</th>
-                                <th>{{ @$datas["assign_data"]["total"] }}元</th>
-                            </tr>
-                            <tr>
-                                <th class="text-center tm-bg-gray" height="50px">訂單備註：</th>
-                                <th>{!! @$datas["assign_data"]["order_remark_format"] !!}</th>
-                            </tr>
-                            @if(@$datas["assign_data"]["status"] == "cancel" && @$datas["assign_data"]["cancel"] == "other" && @$datas["assign_data"]["cancel_remark"] != "")
-                                <tr>
-                                    <th class="text-center tm-bg-gray" height="50px">取消備註：</th>
-                                    <th>{!! @$datas["assign_data"]["cancel_remark_format"] !!}</th>
-                                </tr>
-                            @endif
-                            @if((@$datas["assign_data"]["status"] == "delivery" || @$datas["assign_data"]["status"] == "complete") && @$datas["assign_data"]["delivery_remark"] != "")
-                                <tr>
-                                    <th class="text-center tm-bg-gray" height="50px">出貨備註：</th>
-                                    <th>{!! @$datas["assign_data"]["delivery_remark_format"] !!}</th>
-                                </tr>
-                            @endif
-                        </thead>
-                    </table>
+                    @include('tables.order')
                 </div>
                 <div class="table-responsive">
                     <table class="table table-hover table-striped tm-table-striped-even mt-3"  style="vertical-align: middle;">
