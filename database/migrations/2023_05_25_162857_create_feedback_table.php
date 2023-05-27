1c7cc395896a01e408130848af13512ff4916f86
taii0401 <taii0401@gmail.com>
2023-05-27 13:40:59 +0800

    修改使用者回饋欄位

--- database/migrations/2023_05_25_162857_create_feedback_table.php
+++ database/migrations/2023_05_25_162857_create_feedback_table.php
@@ -18,9 +18,11 @@ class CreateFeedbackTable extends Migration
             $table->string('uuid',50)->comment('uuid');
             $table->string('name',30)->comment('姓名');
             $table->integer('age')->comment('年齡');
+            $table->integer('agree')->default(0)->comment('是否同意：0 否、1 是');
             $table->string('address_zip',5)->nullable()->comment('郵遞區號');
             $table->string('address_county',10)->nullable()->comment('縣市');
             $table->string('address_district',10)->nullable()->comment('鄉鎮市區');
+            $table->longText('content')->nullable()->comment('使用者回饋及感想');
             $table->integer('created_id')->nullable()->comment('建立者id');
             $table->integer('updated_id')->nullable()->comment('修改者id');
             $table->integer('deleted_id')->nullable()->comment('刪除者id');
